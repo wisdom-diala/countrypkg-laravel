9f25322abdcae4d758351812cc289d46e97e3dc1
Diala Wisdom <talk2whizzy@gmail.com>
2021-04-02 08:08:03 +0100

    cleaning up the code

--- src/CountrypkgServiceProvider.php
+++ src/CountrypkgServiceProvider.php
@@ -13,22 +13,7 @@ class CountrypkgServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		$this->loadMigrationsFrom(__DIR__.'/database/migrations');
-		// $this->publishes([
-  //       __DIR__.'/../config/countrypkg.php' => config_path('courier.php'),
-  //   ]);
-		// $jsonString = file_get_contents(__DIR__.'/resources/files/countries.json');
-
-
-  //   	$data = json_decode($jsonString, true);
-
-  //   	dd($data);
-
-		// $jsonString = file_get_contents(__DIR__.'/resources/files/states.json');
-
-
-  //   	$data = json_decode($jsonString, true);
-
-  //   	dd($data);
+		
 	}
 
 	public function register()
