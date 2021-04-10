ff36cd95073dcc656adff4b82f38a4881b23d338
Diala Wisdom <talk2whizzy@gmail.com>
2021-04-10 13:28:55 +0100

    fixing bugs on generating states records

--- src/Console/Commands/GenerateStates.php
+++ src/Console/Commands/GenerateStates.php
@@ -41,32 +41,31 @@ class GenerateStates extends Command
      */
     public function handle()
     {
-        $response = file_get_contents(__DIR__.'/../../resources/files/states.json');
-        $states = json_decode($response);
+    	$response = file_get_contents(__DIR__.'/../../resources/files/states.json');
+		$states = json_decode($response);
        
         $country = $this->argument('country');
         $count_states = 0;
         if ($country == 'all') {
-			$countries = Country::all();
-			if (count($countries) > 0) {
-				foreach ($countries as $country) {
-					if ($states != null) {
-						$no_of_states = count($states);
-						for($i = 0; $i < $no_of_states; $i++) {
-							$check_state = State::where(['name' => $states[$i]->name, 'country_id' => $country->id])->count();
-							if ($check_state <= 0) {
-								State::create([
-									'country_id' => $country->id,
-									'name' => $states[$i]->name
-								]);
-								++$count_states;
-							}
-						}
+        	
+			if ($states != null) {
+				$no_of_states = count($states);
+				for($i = 0; $i < $no_of_states; $i++) {
+					$check_state = State::where(['name' => $states[$i]->name, 'country_id' => $states[$i]->country_id])->count();
+					if ($check_state <= 0) {
+						State::create([
+							'country_id' => $states[$i]->country_id,
+							'name' => $states[$i]->name
+						]);
+						++$count_states;
+						$state_r = $states[$i]->name;
+						$this->info("$state_r");
 					}
 				}
 			}
-		}else{
 			
+
+		}else{
 			if ($states != null) {
 				$no_of_states = count($states);
 				$country_db = Country::where('name', $country)->first();
